f4a67739e41645e23d2cf74470c5ddc54c883bad
Joel <joepotential@gmail.com>
2023-11-23 06:15:35 +0100

    accordion logic done

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const profile = document.querySelector('.profile')
 const logoBtn = document.querySelector('.logo-btn')
 const closeBtn = document.querySelector('.close-btn')
 const extendDiv = document.querySelector('.extend')
+const setupBtn = document.querySelector('.setup-btn')
+const mainBody = document.querySelector('.main-body')
+const main = document.querySelector('.main')
+const steps = document.querySelectorAll('.step')
+const stepBtn = document.querySelectorAll('.step-btn')
 
 
 //HEADER BUTTON FUNCTIONS
@@ -27,4 +32,27 @@ profileBtn.addEventListener('click', function() {
 //CLOSE BUTTON FUNCTION
 closeBtn.addEventListener('click', function() {
     extendDiv.style.display = 'none'
+})
+
+
+//SETUP GUIDE EXPANDER 
+setupBtn.addEventListener('click', function() {
+    mainBody.classList.toggle('close')
+    setupBtn.classList.toggle('close')
+})
+
+
+//ACCORDION LOGIC
+steps.forEach(function(step) {
+    const btn = step.querySelector('.step-main-btn')
+
+    btn.addEventListener('click', function() {
+        steps.forEach(function(item) {
+            if (item !== step) {
+                item.classList.remove('show')
+            }
+        })
+
+        step.classList.toggle('show')
+    })
 })
